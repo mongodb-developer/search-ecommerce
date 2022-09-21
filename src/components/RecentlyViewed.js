eaa2d3c87a3df451e35e19c11921b63fb5cc9a31
Karen Huaulme <karen.huaulme@gmail.com>
2022-09-20 19:52:51 -0500

    sliced array

--- src/components/RecentlyViewed.js
+++ src/components/RecentlyViewed.js
@@ -3,16 +3,27 @@ import Product from "./Product";
 
 const RecentlyViewed = ({
   recentProducts,
-
   setShowProductModal,
   showProductModal,
   setDisplayedProduct,
 }) => {
+  const recentProductsNoRepeat = recentProducts.filter((value, index) => {
+    const _value = JSON.stringify(value);
+    return (
+      index ===
+      recentProducts.findIndex((obj) => {
+        return JSON.stringify(obj) === _value;
+      })
+    );
+  });
+
+  const SixProducts = recentProducts.slice(0, 6);
+
   return (
     <div className="shadow-2xl bg-gradient-to-r from-green-700 to-green-900 w-full rounded my-2 overflow-hidden">
       <div className="text-white text-2xl pl-4 pt-2">Recently Viewed</div>
       <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 px-4 py-2">
-        {recentProducts.map((product, idx) => (
+        {SixProducts.map((product, idx) => (
           <Product
             key={idx}
             index={idx}
@@ -30,50 +41,50 @@ const RecentlyViewed = ({
 
 export default RecentlyViewed;
 
-const recentProducts = [
-  {
-    _id: "6320911ce41329d01899de2f",
-    category: "Clothing",
-    main_image_url: "https://search-demos.s3.us-east-2.amazonaws.com/npm.jpg",
-    marketplace: "Amazon",
-    name: "npm Hat",
-    price: {
-      value: 29,
-      currency: "USD",
-    },
-  },
-  {
-    _id: "6320911ce41329d01899de30",
-    category: "Clothing",
-    main_image_url: "https://search-demos.s3.us-east-2.amazonaws.com/java.jpg",
-    marketplace: "Amazon",
-    name: "Java T-Shirt",
-    price: {
-      value: 39,
-      currency: "USD",
-    },
-  },
-  {
-    _id: "6320911ce41329d01899de31",
-    category: "Clothing",
-    main_image_url:
-      "https://search-demos.s3.us-east-2.amazonaws.com/python.jpg",
-    marketplace: "Amazon",
-    name: "Python T-Shirt",
-    price: {
-      value: 39,
-      currency: "USD",
-    },
-  },
-  {
-    _id: "6320911ce41329d01899de32",
-    category: "Clothing",
-    main_image_url: "https://search-demos.s3.us-east-2.amazonaws.com/vue.jpg",
-    marketplace: "Amazon",
-    name: "Vue T-Shirt",
-    price: {
-      value: 39,
-      currency: "USD",
-    },
-  },
-];
+// const recentProducts = [
+//   {
+//     _id: "6320911ce41329d01899de2f",
+//     category: "Clothing",
+//     main_image_url: "https://search-demos.s3.us-east-2.amazonaws.com/npm.jpg",
+//     marketplace: "Amazon",
+//     name: "npm Hat",
+//     price: {
+//       value: 29,
+//       currency: "USD",
+//     },
+//   },
+//   {
+//     _id: "6320911ce41329d01899de30",
+//     category: "Clothing",
+//     main_image_url: "https://search-demos.s3.us-east-2.amazonaws.com/java.jpg",
+//     marketplace: "Amazon",
+//     name: "Java T-Shirt",
+//     price: {
+//       value: 39,
+//       currency: "USD",
+//     },
+//   },
+//   {
+//     _id: "6320911ce41329d01899de31",
+//     category: "Clothing",
+//     main_image_url:
+//       "https://search-demos.s3.us-east-2.amazonaws.com/python.jpg",
+//     marketplace: "Amazon",
+//     name: "Python T-Shirt",
+//     price: {
+//       value: 39,
+//       currency: "USD",
+//     },
+//   },
+//   {
+//     _id: "6320911ce41329d01899de32",
+//     category: "Clothing",
+//     main_image_url: "https://search-demos.s3.us-east-2.amazonaws.com/vue.jpg",
+//     marketplace: "Amazon",
+//     name: "Vue T-Shirt",
+//     price: {
+//       value: 39,
+//       currency: "USD",
+//     },
+//   },
+// ];
